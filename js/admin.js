365d7f83e6b80ad4a84f02c523c6805c2dd67727
Umar Sarki <sarki201020@gmail.com>
2024-01-21 15:18:51 +0100

    commit 2

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,10 @@
 const asideColl = document.getElementById('aside-coll');
+const catalogBtn = document.getElementById('catalog-btn');
+const catalogDropdown = document.getElementById('catalog-dropdown');
 const aside = document.getElementById('aside');
 const cont = document.getElementsByClassName('container')
+const asideDropdown = document.getElementsByClassName('aside-dropdown')
+const asideDropdownBtn = document.getElementsByClassName('aside-dropdown-btn')
 
 if (asideColl) {
     asideColl.addEventListener('click', () =>{
@@ -11,3 +15,18 @@ if (asideColl) {
 
     })
 }
+if (catalogBtn) {
+    catalogBtn.addEventListener('click', () =>{
+        catalogDropdown.classList.toggle('aside-display');
+    })
+}
+
+for (let i = 0; i <asideDropdownBtn.length; i++) {
+    if(asideDropdownBtn[i]) {
+        asideDropdownBtn[i].addEventListener('click', () =>{
+            asideDropdown[i].classList.toggle('aside-display');
+        })
+    }
+} 
+// asideDropdownBtn[0].getElementsByClassName('icon-more')[0].setAttribute("data-lucide", "chevron-down")
+console.log();
